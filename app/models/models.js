0a5f82de60826611f8066ef9e6813f5f2a4f3945
Austin Kabiru <makabby@gmail.com>
2015-12-29 21:16:22 +0300

    configure environment

--- app/models/models.js
+++ app/models/models.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
 		config = require('../../config');
 
 module.exports = function( wagner ) {
-	mongoose.connect( config.database );
+	mongoose.connect(  process.env.DATABASE_URI || config.databaseLocal );
 
 	// register mongoose as a service
 	wagner.factory( 'db', function() {
